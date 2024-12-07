64972dd30140ea0ea5896385e11873c11fc76ec5
Travellerrr <travelerr11@outlook.com>
2024-12-07 14:08:05 +0800

    refactor: 重构机器人自动审核功能，分成好友审核与群审核两方面

--- src/main/java/cn/travellerr/aronaTools/autoAcceptInvite/CheckInvite.java
+++ src/main/java/cn/travellerr/aronaTools/autoAcceptInvite/CheckInvite.java
@@ -6,148 +6,143 @@ import cn.travellerr.aronaTools.shareTools.Log;
 import cn.travellerr.entity.Favourite;
 import cn.travellerr.utils.FavouriteManager;
 import net.mamoe.mirai.Bot;
-import net.mamoe.mirai.console.permission.AbstractPermitteeId;
-import net.mamoe.mirai.console.permission.PermissionService;
 import net.mamoe.mirai.contact.Group;
-import net.mamoe.mirai.contact.NormalMember;
 import net.mamoe.mirai.contact.User;
 import net.mamoe.mirai.event.events.BotInvitedJoinGroupRequestEvent;
+import net.mamoe.mirai.event.events.NewFriendRequestEvent;
 
 import java.util.ArrayList;
-import java.util.Objects;
+import java.util.List;
 
 import static cn.travellerr.aronaTools.AronaTools.config;
 
 public class CheckInvite {
+
     private static boolean checkMoney(User user) {
         return EconomyUtil.getMoneyByUser(user) >= config.getMoney();
     }
 
-    private static boolean checkLove(User user) {
+    private static boolean checkLove(long user) {
         try {
-            Favourite favourite = FavouriteManager.getInfo(user.getId());
-            if (favourite == null) {
-                return false;
-            }
-            return favourite.getExp() >= config.getLove();
+            Favourite favourite = FavouriteManager.getInfo(user);
+            return favourite != null && favourite.getExp() >= config.getLove();
         } catch (Exception e) {
             return false;
         }
     }
 
-    private static ArrayList<Boolean> checkIsInGroup(User user, Group ARONA, Group Hoshiran) {
-        ArrayList<Boolean> result = new ArrayList<>();
-        result.add(false);
-        result.add(false);
-
-        NormalMember inArona = ARONA.get(user.getId());
-        NormalMember inHoshiran = Hoshiran.get(user.getId());
-
-        if (inArona != null) {
-            result.set(0, true);
+    private static List<Boolean> checkIsInGroup(long user, Group... groups) {
+        List<Boolean> result = new ArrayList<>();
+        for (Group group : groups) {
+            result.add(group.get(user) != null);
         }
-
-        if (inHoshiran != null) {
-            result.set(1, true);
-        }
-
         return result;
     }
 
     public static void check(BotInvitedJoinGroupRequestEvent event) {
         long userId = event.getInvitorId();
         Bot bot = event.getBot();
-        User user = bot.getStranger(userId);
-        boolean isAccept = false;
-        if (user == null) {
-            user = bot.getFriend(userId);
-        }
-
         User owner = bot.getFriend(3132522039L);
         assert owner != null;
 
+        User user = bot.getFriend(userId);
         if (user == null) {
-
-            Log.info("用户邀请机器人加入群聊，获取用户信息为空，拒绝");
-            owner.sendMessage("用户 " + userId + " 邀请" + bot.getNick() +
-                    "加入群聊" + event.getGroupName() + " (" + event.getGroupId() + ") " + "\n" + "由于获取不到该用户信息，已拒绝");
-            event.ignore();
+            user = bot.getStranger(userId);
+            notifyOwnerAndUser(owner, user, event, "请先添加我为好友再邀请我加入群聊哦~");
             return;
         }
 
-        AbstractPermitteeId.ExactUser exactUser = PermissionController.getUserPermittee(user);
-        if (exactUser != null && PermissionService.hasPermission(exactUser, PermissionController.inviteBypassPermission)) {
-            event.accept();
-            Log.info("用户邀请机器人加入群聊，已通过权限检查，已接受");
-
-            owner.sendMessage("用户 " + user.getNick() + " (" + user.getId() + ") 邀请" + bot.getNick() +
-                    "加入群聊" + event.getGroupName() + " (" + event.getGroupId() + ") " + "\n");
-
-            user.sendMessage("你邀请" + bot.getNick() +
-                    "加入群聊" + event.getGroupName() + " (" + event.getGroupId() + ") " + "\n");
-
+        if (PermissionController.hasPermission(user, PermissionController.addFriendBypassPermission)) {
+            acceptInvite(event, owner, user);
             return;
-
         }
+
         boolean isMoneyEnough = checkMoney(user);
-        boolean isLoveEnough = checkLove(user);
+        List<Boolean> isInOneOfGroup = checkIsInGroup(userId, getGroups(bot));
+        boolean isInGroup = isInOneOfGroup.contains(true);
 
-        Group ARONA = bot.getGroup(config.getGroup().get(0));
-        Group Hoshiran = bot.getGroup(config.getGroup().get(1));
-
-        assert ARONA != null && Hoshiran != null;
-
-        ArrayList<Boolean> isInOneOfGroup = checkIsInGroup(user, ARONA, Hoshiran);
-
-        boolean isInGroup = isInOneOfGroup.get(0) || isInOneOfGroup.get(1);
-
-
-        if (isMoneyEnough && isLoveEnough && isInGroup) {
+        if (isMoneyEnough && isInGroup) {
             EconomyUtil.plusMoneyToUser(user, -config.getMoney());
             event.accept();
-            isAccept = true;
+            notifyOwnerAndUser(owner, user, event, "通过");
         } else {
             event.ignore();
+            notifyOwnerAndUser(owner, user, event, "未通过审核\n请尝试以下方法: \n" + getTryInviteGroupMsg(isMoneyEnough, isInGroup, user));
+        }
+    }
 
+    public static void checkFriendRequest(NewFriendRequestEvent event) {
+        long userId = event.getFromId();
+        Bot bot = event.getBot();
+        User owner = bot.getFriend(3132522039L);
+        assert owner != null;
+
+
+        if (PermissionController.hasPermission(userId, PermissionController.inviteBypassPermission)) {
+            acceptFriendRequest(event, owner);
+            return;
         }
 
+        boolean isLoveEnough = checkLove(userId);
+        List<Boolean> isInOneOfGroup = checkIsInGroup(userId, getGroups(bot));
+        boolean isInGroup = isInOneOfGroup.contains(true);
 
+        if (isLoveEnough && isInGroup) {
+            event.accept();
+            notifyOwnerAndUser(owner, event, "通过");
+        } else {
+            event.reject(false);
+            notifyOwnerAndUser(owner, event, "未通过审核\n请尝试以下方法: \n" + getTryAddFriendMsg(isLoveEnough, isInGroup, userId));
+        }
+    }
+
+    private static Group[] getGroups(Bot bot) {
+        return new Group[]{bot.getGroup(config.getGroup().get(0)), bot.getGroup(config.getGroup().get(1))};
+    }
+
+
+
+    private static void acceptInvite(BotInvitedJoinGroupRequestEvent event, User owner, User user) {
+        event.accept();
+        notifyOwnerAndUser(owner, user, event, "通过");
+    }
+
+    private static void acceptFriendRequest(NewFriendRequestEvent event, User owner) {
+        event.accept();
+        notifyOwnerAndUser(owner, event, "通过");
+    }
+
+    private static void notifyOwnerAndUser(User owner, User user, BotInvitedJoinGroupRequestEvent event, String message) {
+        String msg = "用户 " + event.getInvitorNick() + " (" + event.getInvitorId() + ") 邀请" + event.getBot().getNick() + "加入群聊" + event.getGroupName() + " (" + event.getGroupId() + ") " + "\n" + message;
+        Log.info(msg);
+        owner.sendMessage(msg);
+        if (user != null) user.sendMessage(msg);
+    }
+
+    private static void notifyOwnerAndUser(User owner, NewFriendRequestEvent event, String message) {
+        String msg = "用户 " + event.getFromNick() + " (" + event.getFromId() + ") 申请添加" + event.getBot().getNick() + "为好友\n" + message;
+        Log.info(msg);
+        owner.sendMessage(msg);
+        User user = event.getBot().getFriend(event.getFromId());
+        if (user != null) user.sendMessage(msg);
+    }
+
+    private static String getTryInviteGroupMsg(boolean isEnough, boolean isInGroup, User user) {
         String moneyMsg = "金币数量(使用#个人信息 查看) >= 300 (目前 %金币%)";
-
-        String loveMsg = "好感经验(使用#好感度 查看) >= " + config.getLove() + " (目前 %好感度% 经验)";
-
         String groupMsg = "尝试加入 什亭之匣群聊(626860767)";
-
-        Favourite favourite = FavouriteManager.getInfo(user.getId());
-
-        long userExp = 0;
-        if (favourite != null) userExp = favourite.getExp();
-
-        String tryMsg = (isMoneyEnough ? "" : (replaceVar(moneyMsg, "金币", EconomyUtil.getMoneyByUser(user)) + "\n"))
-                + (isLoveEnough ? "" : (replaceVar(loveMsg, "好感度", userExp) + "\n"))
+        return (isEnough ? "" : (replaceVar(moneyMsg, "金币", EconomyUtil.getMoneyByUser(user)) + "\n"))
                 + (isInGroup ? "" : (groupMsg + "\n"));
+    }
 
-        String BotVerifyMsg = (isAccept ? "通过" : ("未通过" + "审核"
-                + "\n" + "原因: " + (isMoneyEnough ? "" : "余额不足 ") + (isLoveEnough ? "" : "好感不足 ") + (isInGroup ? "" : "不在群聊中 ")
-                + "未通过审核\n请尝试以下方法: \n" + tryMsg));
-
-        Log.info("用户 " + user.getNick() + " (" + user.getId() + ") 邀请" + bot.getNick() +
-                "加入群聊" + event.getGroupName() + " (" + event.getGroupId() + ") " + "\n"
-                + BotVerifyMsg);
-
-        owner.sendMessage("用户 " + user.getNick() + " (" + user.getId() + ") 邀请" + bot.getNick() +
-                "加入群聊" + event.getGroupName() + " (" + event.getGroupId() + ") " + "\n"
-                + BotVerifyMsg);
-
-        user.sendMessage("你邀请" + bot.getNick() +
-                "加入群聊" + event.getGroupName() + " (" + event.getGroupId() + ") " + "\n"
-                + BotVerifyMsg);
-
-        Group group = isInOneOfGroup.get(0) ? ARONA : (isInOneOfGroup.get(1) ? Hoshiran : null);
-
-        Objects.requireNonNullElse(group, Hoshiran).sendMessage("用户 " + user.getNick() + " (" + user.getId() + ") 邀请" + bot.getNick() +
-                "加入群聊\n"
-                + BotVerifyMsg);
+    private static String getTryAddFriendMsg(boolean isLoveEnough, boolean isInGroup, long userId) {
+        String loveMsg = "好感经验(使用#好感度 查看) >= " + config.getLove() + " (目前 %好感度% 经验)";
+        String groupMsg = "尝试加入 什亭之匣群聊(626860767)";
+        Favourite favourite = FavouriteManager.getInfo(userId);
+        if (favourite == null) {
+            favourite = Favourite.builder().exp(0L).build();
+        }
+        return (isLoveEnough ? "" : (replaceVar(loveMsg, "好感度", favourite.getExp()) + "\n"))
+                + (isInGroup ? "" : (groupMsg + "\n"));
     }
 
     public static String replaceVar(String msg, String varName, Object value) {

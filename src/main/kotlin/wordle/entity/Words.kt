60e285a3a8106d5ded713a2bae6d51695e3af8b5
Travellerrr <travelerr11@outlook.com>
2024-12-21 17:58:03 +0800

    feat: 将wordle词库改为CET4-6 fix: 修复wordle简单版图像

--- src/main/kotlin/wordle/entity/Words.kt
+++ src/main/kotlin/wordle/entity/Words.kt
@@ -5,18 +5,32 @@ import kotlinx.serialization.json.Json
 import java.io.InputStreamReader
 
 @Serializable
-data class Words(
-    val valid: List<String>,
-    val words: List<String>
+data class WordList (
+    val words: List<Words>,
+    val valid: List<String>
 ) {
     companion object {
 
-        fun parse(inputStream: InputStreamReader): Words {
+        fun parse(inputStream: InputStreamReader): WordList {
             return Json.decodeFromString(serializer(), inputStream.readText())
         }
     }
-    fun getRandomWord(): String {
+    fun getRandomWord(): Words {
         val random = words.indices.random()
         return words[random]
+    }
+}
+
+@Serializable
+data class Words(
+/*    val valid: List<String>,
+    val words: List<String>*/
+    val word: String,
+    val chDef: String,
+    val enDef: String
+
+) {
+    fun getMessage(): String {
+        return "$word\n中文释义：$chDef"
     }
 }

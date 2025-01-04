a641bfdcbd6dc83dcd4b1e7bbfcdb9f14a04d8e2
Travellerrr <travelerr11@outlook.com>
2025-01-04 19:56:15 +0800

    fix: 修复过时的菜单文件路径

--- src/main/java/cn/travellerr/aronaTools/autoAcceptInvite/SendMenu.java
+++ src/main/java/cn/travellerr/aronaTools/autoAcceptInvite/SendMenu.java
@@ -1,5 +1,6 @@
 package cn.travellerr.aronaTools.autoAcceptInvite;
 
+import cn.travellerr.aronaTools.AronaTools;
 import net.mamoe.mirai.contact.Contact;
 import net.mamoe.mirai.event.events.BotJoinGroupEvent;
 import net.mamoe.mirai.event.events.FriendAddEvent;
@@ -17,7 +18,7 @@ public class SendMenu {
             Contact subject = event.getUser();
 
             // 菜单图片路径（相对路径）
-            File menu = new File("./data/image/menu/menu1.png");
+            File menu = AronaTools.INSTANCE.resolveDataFile(AronaTools.menuConfig.getMenus().get(0));
             try (ExternalResource resource = ExternalResource.create(menu)) {
                 Image image = subject.uploadImage(resource);
 
@@ -43,7 +44,7 @@ public class SendMenu {
             Contact subject = event.getGroup();
 
             // 菜单图片路径（相对路径）
-            File menu = new File("./data/image/menu/menu1.png");
+            File menu = AronaTools.INSTANCE.resolveDataFile(AronaTools.menuConfig.getMenus().get(0));
             try (ExternalResource resource = ExternalResource.create(menu)) {
                 Image image = subject.uploadImage(resource);
 
